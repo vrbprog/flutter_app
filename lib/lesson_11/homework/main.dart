507256548aeb51789b1b4a787dcf6b22b45dd55a
Roman <vrbox@i.ua>
2026-04-17 17:37:44 +0300

    feat: add Subtask4

--- lib/lesson_11/homework/main.dart
+++ lib/lesson_11/homework/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const Scaffold(body: Subtask3()),
+      home: const Scaffold(body: Subtask4()),
     );
   }
 }
@@ -105,6 +105,47 @@ class Subtask3 extends StatelessWidget {
             colorStar: Colors.yellow,
           ),
         ],
+      ),
+    );
+  }
+}
+
+class Subtask4 extends StatelessWidget {
+  const Subtask4({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.center,
+      child: Padding(
+        padding: const EdgeInsets.only(bottom: 180.0),
+        child: Column(
+          spacing: 8,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: const [
+            Expanded(
+              flex: 1,
+              child: MyCard(
+                label: 'Привіт Flutter!',
+                colorStar: Colors.yellow,
+                mainAlig: MainAxisAlignment.start,
+                crossAlig: CrossAxisAlignment.start,
+              ),
+            ),
+            MyCard(
+              label: 'Привіт Flutter!',
+              color: Colors.lightGreen,
+              colorStar: Colors.yellow,
+              mainAlig: MainAxisAlignment.center,
+              crossAlig: CrossAxisAlignment.center,
+            ),
+            MyCard(
+              label: 'Привіт Flutter!',
+              color: Colors.red,
+              colorStar: Colors.yellow,
+            ),
+          ],
+        ),
       ),
     );
   }
