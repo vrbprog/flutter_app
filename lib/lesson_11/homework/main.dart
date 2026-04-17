bc2057c2f62a709a29c5a6bf56b3f0bcc410ec54
Roman <vrbox@i.ua>
2026-04-17 17:39:54 +0300

    feat: add Subtask5

--- lib/lesson_11/homework/main.dart
+++ lib/lesson_11/homework/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const Scaffold(body: Subtask4()),
+      home: const Scaffold(body: Subtask5()),
     );
   }
 }
@@ -140,6 +140,133 @@ class Subtask4 extends StatelessWidget {
               crossAlig: CrossAxisAlignment.center,
             ),
             MyCard(
+              label: 'Привіт Flutter!',
+              color: Colors.red,
+              colorStar: Colors.yellow,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MyClicableCard extends StatefulWidget {
+  const MyClicableCard({
+    super.key,
+    this.color = Colors.blue,
+    this.label,
+    this.colorStar = Colors.transparent,
+    this.mainAlig = MainAxisAlignment.end,
+    this.crossAlig = CrossAxisAlignment.end,
+    this.clicable = true,
+  });
+
+  final Color color;
+  final String? label;
+  final Color colorStar;
+  final MainAxisAlignment mainAlig;
+  final CrossAxisAlignment crossAlig;
+  final bool clicable;
+
+  @override
+  State<MyClicableCard> createState() => _MyClicableCardState();
+}
+
+class _MyClicableCardState extends State<MyClicableCard> {
+  bool _isPressed = false;
+
+  Color _darkenColor(Color color) {
+    return Color.lerp(color, Colors.black, 0.2) ?? color;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final displayColor = _isPressed ? _darkenColor(widget.color) : widget.color;
+    final shadowColor = _isPressed
+        ? Colors.black.withValues(alpha: .5)
+        : Colors.transparent;
+    final iconType = _isPressed ? Icons.star_border : Icons.star;
+
+    return GestureDetector(
+      onTapDown: (details) {
+        if (widget.clicable) {
+          setState(() {
+            _isPressed = true;
+          });
+        }
+      },
+      onTapUp: (details) {
+        if (widget.clicable) {
+          setState(() {
+            _isPressed = false;
+          });
+        }
+      },
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 300),
+        padding: const EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: displayColor,
+          borderRadius: BorderRadius.circular(16),
+          boxShadow: [
+            BoxShadow(
+              color: shadowColor,
+              blurRadius: 8,
+              offset: const Offset(0, 6),
+            ),
+          ],
+        ),
+        width: 300,
+        height: 150,
+        child: Row(
+          mainAxisAlignment: widget.mainAlig,
+          crossAxisAlignment: widget.crossAlig,
+          spacing: 10,
+          children: [
+            Icon(iconType, color: widget.colorStar, size: 24),
+            Text(
+              widget.label ?? '',
+              style: const TextStyle(fontSize: 24, color: Colors.white),
+            ),
+            Icon(iconType, color: widget.colorStar, size: 24),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Subtask5 extends StatelessWidget {
+  const Subtask5({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.center,
+      child: Padding(
+        padding: const EdgeInsets.only(bottom: 180.0),
+        child: Column(
+          spacing: 8,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: const [
+            Expanded(
+              flex: 1,
+              child: MyClicableCard(
+                label: 'Привіт Flutter!',
+                colorStar: Colors.yellow,
+                mainAlig: MainAxisAlignment.start,
+                crossAlig: CrossAxisAlignment.start,
+              ),
+            ),
+            MyClicableCard(
+              label: 'Привіт Flutter!',
+              color: Colors.lightGreen,
+              colorStar: Colors.yellow,
+              mainAlig: MainAxisAlignment.center,
+              crossAlig: CrossAxisAlignment.center,
+            ),
+            MyClicableCard(
               label: 'Привіт Flutter!',
               color: Colors.red,
               colorStar: Colors.yellow,
